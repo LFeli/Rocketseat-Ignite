c7ed87d1f74aed7a59755a8c567bcd266c93efac
Lucas Felipe <lucasfelipemk@gmail.com>
2023-11-02 19:55:40 -0300

    feat: module 01: fundamentos dos tailwind, done

--- masterizando-o-tailwind/src/app/page.tsx
+++ masterizando-o-tailwind/src/app/page.tsx
@@ -1,5 +1,14 @@
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <div className="p-8 bg-slate-50 dark:bg-slate-900 text-slate-900 dark:text-slate-100 h-screen flex flex-col items-center text-center">
+      <div className="max-w-2xl">
+        <h1 className="font-bold text-3xl sm:text-4xl lg:text-5xl">Rapidly build modern websites without ever leaving your HTML.</h1>
+        <p className="mt-4 dark:text-slate-400">A utility-first CSS framework packed with classes like flex, pt-4, text-center and rotate-90 that can be composed to build any design, directly in your markup.</p>
+        <button className="bg-sky-500 dark:bg-sky-400 text-white dark:text-sky-950 enabled:hover:bg-sky-700 transition-colors px-10 py-2 rounded font-medium mt-4 disabled:opacity-60 disabled:cursor-not-allowed">
+          Sign in
+        </button>
+      </div>
+
+    </div>
   )
 }
