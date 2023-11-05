da40dd1e32b018a000043aef89139a670874c8b9
Lucas Felipe <lucasfelipemk@gmail.com>
2023-11-05 12:29:42 -0300

    feat: created file input component with composition pattern and image preview it's working

---
+++ masterizando-o-tailwind/src/components/Form/FileInput/Index.tsx
@@ -0,0 +1,6 @@
+import { Root } from './Root'
+import { ImagePreview } from './ImagePreview'
+import { Trigger } from './Trigger'
+import { Control } from './Control'
+
+export { Root, ImagePreview, Trigger, Control }
