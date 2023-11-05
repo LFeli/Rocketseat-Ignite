57199939bea2dfabfbe044c94fa16c812d0be46c
Lucas Felipe <lucasfelipemk@gmail.com>
2023-11-05 15:00:03 -0300

    feat: created file list component and added the stucuture of file input component

--- masterizando-o-tailwind/src/components/Form/FileInput/Index.tsx
+++ masterizando-o-tailwind/src/components/Form/FileInput/Index.tsx
@@ -2,5 +2,6 @@ import { Root } from './Root'
 import { ImagePreview } from './ImagePreview'
 import { Trigger } from './Trigger'
 import { Control } from './Control'
+import { FileList } from './FileList'
 
-export { Root, ImagePreview, Trigger, Control }
+export { Root, ImagePreview, Trigger, Control, FileList }
