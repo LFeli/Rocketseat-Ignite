73cd7c5fcd8751789a1dcdea06fbd610666fa541
Lucas Felipe <lucasfelipemk@gmail.com>
2023-11-05 15:00:24 -0300

    feat: added file list component in field on form

--- masterizando-o-tailwind/src/app/page.tsx
+++ masterizando-o-tailwind/src/app/page.tsx
@@ -158,6 +158,7 @@ export default function Home() {
 
             <FileInput.Root>
               <FileInput.Trigger />
+              <FileInput.FileList />
               <FileInput.Control multiple />
             </FileInput.Root>
           </div>
