51b487f50abcc8c5062b951d8c769da5631ff085
Lucas Felipe <lucasfelipemk@gmail.com>
2023-11-05 17:11:35 -0300

    little alterations...

--- masterizando-o-tailwind/src/components/SideBar/Profile.tsx
+++ masterizando-o-tailwind/src/components/SideBar/Profile.tsx
@@ -2,13 +2,12 @@ import { LogOut } from 'lucide-react'
 
 export function Profile() {
   return (
-    <div className="grid-cols-profile grid items-center gap-3">
+    <div className="grid grid-cols-profile items-center gap-3">
       <img
         src="https://github.com/Lfeli.png"
         className="h-10 w-10 rounded-full"
         alt=""
       />
-
       <div className="flex flex-1 flex-col truncate">
         <span className="text-sm font-semibold text-zinc-700">
           Lucas Felipe
@@ -17,7 +16,6 @@ export function Profile() {
           lucas.felipe@email.com
         </span>
       </div>
-
       <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
         <LogOut className="h-5 w-5 text-zinc-500" />
       </button>
