0f7ace5cc78cb2bbd9f3d5d38f2be54e33d74d76
Lucas Felipe <lucasfelipemk@gmail.com>
2023-11-08 21:55:10 -0300

    feat: adding dark mode for sideBar component

--- masterizando-o-tailwind/src/components/SideBar/Profile.tsx
+++ masterizando-o-tailwind/src/components/SideBar/Profile.tsx
@@ -10,10 +10,10 @@ export function Profile() {
         alt=""
       />
       <div className="flex flex-1 flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
           Lucas Felipe
         </span>
-        <span className="truncate text-sm text-zinc-500">
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
           lucas.felipe@email.com
         </span>
       </div>
